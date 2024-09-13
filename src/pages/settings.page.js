a6ac2458dc5ce1c98f877dd33ff51f07bb7e146a
Lida <l.karpova@freshauto2.ru>
2024-09-13 13:53:33 +0300

    profile/t

--- src/pages/settings.page.js
+++ src/pages/settings.page.js
@@ -5,6 +5,7 @@ constructor (page) {
     super(page);
     this.updateButton = page.getByRole('button','Update Settings');
     this.bioField = this.page.getByPlaceholder('Short bio about you');
+    this.logoutButton = this.page.getByRole('link', { name: 'Logout' });
 }
 
 async enterUserBio (bio) {
@@ -26,6 +27,10 @@ async updateProfile (bio = '') {
     await this.bioField.click();
     await this.bioField.fill(bio);
     await this.updateButton.click();
+}
+
+async goToLogout() {
+    await this.logoutButton.click()
 }
 }
 
